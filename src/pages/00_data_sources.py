06281bd7fccbe58d43c32c0f18d89c6563a24ab4
Darius Murawski <darius@murawski.blog>
2024-11-15 19:40:46 +0100

    begin handle table overwrite

--- src/pages/00_data_sources.py
+++ src/pages/00_data_sources.py
@@ -7,52 +7,44 @@ import glob
 def get_files(suffix = '*.csv'):
     return glob.glob(os.path.join("data", suffix), recursive=True)
 
-def process_csv():
-    csv_files = get_files("*.csv")
-    csv_file = st.selectbox("Select a CSV file", csv_files)
-    if st.button("Register Table", key="register_table"):
-        df = pd.read_csv(csv_file)
-        table_name = csv_file.split(os.sep)[-1].replace(".csv", "")
-        with duckdb.connect("database.duckdb", read_only=False) as con:
-            con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM df")
-            result = con.execute(f"SELECT COUNT(*) as CNT FROM {table_name}").fetchone()
-            for r in result:
-                st.write(f"Table {table_name} has {r} rows.")
+def create_table(con, table_name, df):
+    con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM df")
+    result = con.execute(f"SELECT COUNT(*) as CNT FROM {table_name}").fetchone()
+    for r in result:
+        st.success(f"Table {table_name} created with {r} rows.")
 
-def process_jsonl():
-    jsonl_files = get_files("*.jsonl")
-    jsonl_file = st.selectbox("Select a JSONL file", jsonl_files)
+def process(file_type):
+    files = get_files(f"*.{file_type}")
+    file = st.selectbox(f"Select a {file_type.upper()} file", files)
     if st.button("Register Table", key="register_table"):
-        df = pd.read_json(jsonl_file, lines=True)
-        table_name = jsonl_file.split(os.sep)[-1].replace(".jsonl", "")
-        with duckdb.connect("database.duckdb", read_only=False) as con:
-            con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM df")
-            result = con.execute(f"SELECT COUNT(*) as CNT FROM {table_name}").fetchone()
-            for r in result:
-                st.write(f"Table {table_name} has {r} rows.")
+        if "csv" == file_type:
+            df = pd.read_csv(file)
+        elif "jsonl" == file_type or "json" == file_type:
+            is_jsonl = "jsonl" == file_type
+            df = pd.read_json(file, lines=is_jsonl)
+        else:
+            st.error(f"Unsupported file type: {file_type}")
+            return
+        table_name = file.split(os.sep)[-1].replace(f".{file_type}", "")
 
-def process_json():
-    json_files = get_files("*.json")
-    json_file = st.selectbox("Select a JSON file", json_files)
-    if st.button("Register Table", key="register_table"):
-        df = pd.read_json(json_file, lines=False)
-        table_name = json_file.split(os.sep)[-1].replace(".json", "")
         with duckdb.connect("database.duckdb", read_only=False) as con:
-            con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM df")
-            result = con.execute(f"SELECT COUNT(*) as CNT FROM {table_name}").fetchone()
-            for r in result:
-                st.write(f"Table {table_name} has {r} rows.")
+            query = f"SELECT COUNT(*) FROM information_schema.tables WHERE table_name = '{table_name}'"
+            result = con.execute(query).fetchone()
+            with st.form(key="create_table"):
+                if result[0] > 0:
+                    st.write(f"Table {table_name} already exists. Overwrite?")
+                    if st.form_submit_button("Overwrite Table!"):
+                        con.execute(f"DROP TABLE {table_name}")
+                        create_table(con, table_name, df)
+                    else:
+                        st.warning("Table not overwritten.")
+                else:
+                    create_table(con, table_name, df)
+
 
 st.write("Hello World!")
 all_data_sources = ["CSV [Comma Separated Values]", "JSONL [JSON Lines]", "JSON [JavaScript Object Notation]"]
 
 data_source = st.selectbox("Select a Source", all_data_sources, key="source")
 source = data_source.split(" ")[0]
-if "CSV" == source:
-    process_csv()
-elif "JSONL" == source:
-    process_jsonl()
-elif "JSON" == source:
-    process_json()
-else:
-    st.write("Unsupported data source")
+process(source.lower())
